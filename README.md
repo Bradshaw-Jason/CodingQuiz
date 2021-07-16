b9df375ee02867df2bb98fb3d0ea6b10b4010c0d
Jason Bradshaw <jasoncbradshaw97@gmail.com>
2021-07-15 22:16:34 -0700

    added timer functionaly and abilty to save and view score

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 This application will time the user as they attempt to answer a series of coding questions.
 save your high score and see how many questions you can answer. be carful because missing a questtion will decrease you time
 
+github repo: https://github.com/Bradshaw-Jason/CodingQuiz
+live address: https://bradshaw-jason.github.io/CodingQuiz/
 
 ## This application is currentlly not in working order
 current udaates needed to be made include
--timer function
--requesting and saving high scores
+-subtracting time for an incorrect answer
+-saving multiple highscore
 -styling
+-additional coding questions
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,33 @@
-
-
+var highscoreEl = $('#highscores');
+var introEl = $('.intro');
 var question2El = $('#question2');
 var question1El = $('#question1');
 var startBtnEl = $('#startBtn');
 var answerBtnEl = $('.answerBtn');
+var timerEl = $('#timer');
 
 var questionList = [question1El, question2El];
 var questionNum = 0;
 var score = 0;
+var currentQuestion;
+var timerCount;
+var timer;
+var quizComleted;
+var savedInitials="no saved initals";
+var csavedScore="no saved score";
 
 // start the quiz
 startBtnEl.on('click', function () {
-    var time = 20;
     score = 0;
+    timerCount = 10
+    startTimer()
+    introEl.css({ 'display': 'none' });
     displayQuestion();
 });
 
 //Show question then 
 function displayQuestion() {
-    var currentQuestion = questionList[questionNum];
+    currentQuestion = questionList[questionNum];
     currentQuestion.css({ 'display': 'block' });
 };
 
@@ -37,3 +46,38 @@ answerBtnEl.on('click', function(){
 });
 
 
+//timer function
+function startTimer() {
+    timer = setInterval(function() {
+    timerCount--;
+    timerEl.text("Time remaining: "+timerCount);
+
+    if(timerCount===0){
+        clearInterval(timer);
+        quizOver();
+    }
+
+
+    }, 1000);
+  };
+
+  function quizOver(){
+    currentQuestion.css({ 'display': 'none' });
+    introEl.css({ 'display': 'block' });
+    timerEl.text("Time remaining: 10");
+    console.log("game over")
+
+    var initials=prompt("Enter your initials to save your highscore");
+    if(initials == null || initials == ""){
+        return;
+    }
+    else{
+        localStorage.setItem('savedInitials', initials);
+        localStorage.setItem('savedScore',score);
+    }
+        
+  }
+  
+  highscoreEl.on('click', function(){
+      alert('HIGHSCORE!\nInitials: '+localStorage.getItem('savedInitials')+'\nScore: '+localStorage.getItem('savedScore'));
+  });
